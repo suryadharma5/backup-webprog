58b0a83cf63dc5563df9dacd2ef8faeaa61faa08
vincavianny <vicensia20@gmail.com>
2023-06-26 13:25:56 +0700

    update db replies

--- database/seeders/RepliesSeeder.php
+++ database/seeders/RepliesSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
 
 class RepliesSeeder extends Seeder
 {
